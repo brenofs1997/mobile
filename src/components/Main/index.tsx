f0fe38aa0499e3f61348ed7a13a023028508e342
PauloFH99 <brenofs1997@gmail.com>
2022-12-06 22:49:18 -0300

    Finalizando Projeto

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,37 +1,182 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { CartItem } from '../../types/CartItem';
+import { Product } from '../../types/product';
 import { Button } from '../Button';
+import { Cart } from '../Cart';
 import { Categories } from '../Categories';
 import { Header } from '../Header';
+import { Empty } from '../Icons/Empty';
 import { Menu } from '../Menu';
 import { TableModal } from '../TableModal';
-import { Container ,CategoriesContainer,MenuContainer,Footer, FooterContainer} from './styles';
+import { Text } from '../Text';
+import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer, CenteredContainer } from './styles';
+import { Category } from '../../types/Category';
+import { api } from '../../utils/api';
+
 
 export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false);
-  const [selectedTable, setSelectedTable]= useState('');
+  const [selectedTable, setSelectedTable] = useState('');
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [isLoading,setIsLoading] = useState(false);
+  const [products,setProducts] = useState<Product[]>([]);
+  const [categories,setCategories] = useState<Category[]>([]);
+  const [isLoadingProducts,setIsLoadingProducts] = useState(false);
 
-  function handleSaveTable(table: string){
+  useEffect(() => {
+    Promise.all([
+      api.get('/categories'),
+      api.get('/products'),
+    ]).then(([categoriesResponse, productsResponse]) => {
+
+      setCategories(categoriesResponse.data);
+      setProducts(productsResponse.data);
+      setIsLoading(false);
+    });
+  },[]);
+
+  async function handleSelectCategory(categoryId:string) {
+    const route = !categoryId
+      ? '/products'
+      : `/categories/${categoryId}/products`;
+
+    setIsLoadingProducts(true);
+
+    const {data} = await  api.get(route);
+
+    setProducts(data);
+    setIsLoadingProducts(false);
+  }
+
+  function handleSaveTable(table: string) {
     setSelectedTable(table);
+    setIsTableModalVisible(false);
+  }
+
+  function handleResetOrder() {
+    setSelectedTable('');
+    setCartItems([]);
+  }
+
+  function handleAddToCart(product: Product) {
+    if (!selectedTable) {
+      setIsTableModalVisible(true);
+    }
+
+    setCartItems((prevState) => {
+      const itemIndex = prevState.findIndex(cartItem => cartItem.product._id === product._id);
+
+      if (itemIndex < 0) {
+        return prevState.concat({
+          quantity: 1,
+          product,
+        });
+      }
+
+      const newCartItems = [...prevState];
+      const item = newCartItems[itemIndex];
+
+      newCartItems[itemIndex] = {
+        ...item,
+        quantity: item.quantity + 1,
+      };
+
+      return newCartItems;
+    });
+  }
+
+  function handleDecrementCartItem(product: Product) {
+    setCartItems((prevState) => {
+      const itemIndex = prevState.findIndex(cartItem => cartItem.product._id === product._id);
+
+      const item = prevState[itemIndex];
+      const newCartItems = [...prevState];
+
+      if (item.quantity === 1) {
+        newCartItems.splice(itemIndex, 1);
+
+        return newCartItems;
+      }
+
+      newCartItems[itemIndex] = {
+        ...item,
+        quantity: item.quantity - 1,
+      };
+
+      return newCartItems;
+    });
   }
 
   return (
     <>
       <Container>
-        <Header/>
-        <CategoriesContainer>
-          <Categories />
-        </CategoriesContainer>
-        <MenuContainer>
-          <Menu />
-        </MenuContainer>
+        <Header
+          selectedTable={selectedTable}
+          onCancelOrder={handleResetOrder}
+        />
+
+        {isLoading && (
+          <CenteredContainer>
+            <ActivityIndicator color="#D73035" size="large"/>
+          </CenteredContainer>
+        )}
+
+        {!isLoading && (
+          <>
+            <CategoriesContainer>
+              <Categories
+                categories={categories}
+                onSelectCategory={handleSelectCategory}
+              />
+            </CategoriesContainer>
+
+            {isLoadingProducts ? (
+              <CenteredContainer>
+                <ActivityIndicator color="#D73035" size="large"/>
+              </CenteredContainer>
+            ):(
+              <>
+                {products.length > 0  ? (
+                  <MenuContainer>
+                    <Menu
+                      onAddToCart={handleAddToCart}
+                      products={products}
+                    />
+                  </MenuContainer>
+                ) : (
+                  <CenteredContainer>
+                    <Empty />
+                    <Text color="#666" style={{marginTop:24}}>Nenhum produto foi encontrado!</Text>
+                  </CenteredContainer>
+                )}
+              </>
+            )}
+
+
+          </>
+        )}
       </Container>
 
       <Footer>
         <FooterContainer>
           {!selectedTable && (
-            <Button onPress={() => setIsTableModalVisible(true)} >
-                Novo Pedido
+            <Button
+              onPress={() => setIsTableModalVisible(true)}
+              disabled={isLoading}
+            >
+                            Novo Pedido
             </Button>
+          )}
+
+          {selectedTable && (
+            <Cart
+              cartItems={cartItems}
+              onAdd={handleAddToCart}
+              onDecrement={handleDecrementCartItem}
+              onConfirmOrder={handleResetOrder}
+              selectedTable={selectedTable}
+            />
           )}
         </FooterContainer>
       </Footer>
